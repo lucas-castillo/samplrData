93d75903fab95b881f040a9d87390fa4b702ff5c
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-05-11 18:15:32 +0100

    upload e4 of sundh et al 2023

--- R/data.R
+++ R/data.R
@@ -17,3 +17,22 @@
 #' \insertRef{sundh2023UnifiedExplanationVariability}{samplrData}
 #'
 "sundh2023e3"
+
+#' Data from Experiment 4 in \insertCite{sundh2023UnifiedExplanationVariability;textual}{samplrData}
+#'
+#' Participants made probability judgments about future hypothetical events, of the format: “What is the probability that there will be an early UK general election AND the UK economy will recover this year?". The experiment consisted of three blocks, so that all participants responded to each unique query three times.
+#'
+#' This data are licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/), reproduced from materials in [OSF](https://osf.io/9kea6/).
+#' \describe{
+#'   \item{ID}{}
+#'   \item{block}{3 blocks in total}
+#'   \item{trial}{Trial Number within a block}
+#'   \item{query, querydetail}{Verbal descriptions of the query}
+#'   \item{querytype}{Type of query: e.g. not B given A = p(¬B|A)}
+#'   \item{queryset}{Whether the query is about biden and 2050 climate goals or UK election and economic recovery}
+#'   \item{Estimate}{Estimated probability, in percentages}
+#' }
+#' @references
+#' \insertRef{sundh2023UnifiedExplanationVariability}{samplrData}
+#'
+"sundh2023e4"
