9b35fe1bd84f38f602073e10e6fe95c4ace56a6a
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-06-08 20:58:45 +0100

    add zhu2020 data

--- R/data.R
+++ R/data.R
@@ -1,3 +1,43 @@
+#' Data from Experiment 1 in \insertCite{zhu2020BayesianSamplerGeneric;textual}{samplrData}
+#'
+#' Participants made probability judgments of the format: “What is the probability that the weather is \[X\] on a random day in England?". Various weather events were used, and the queries included both marginal events, conditional events, conjunctions, and disjunctions. The total set of 20 unique queries formed a block within which the presentation order was randomized for each participant. The experiment consisted of three blocks, so that all participants responded to each unique query three times.
+#'
+#' This data are licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/), reproduced from materials in [OSF](https://osf.io/mgcxj/).
+#' \describe{
+#'   \item{ID}{}
+#'   \item{block}{3 blocks in total}
+#'   \item{trial}{Trial Number within a block}
+#'   \item{query, querydetail}{Verbal descriptions of the query}
+#'   \item{querytype}{Type of query: e.g. notBgA = p(¬B|A)}
+#'   \item{Estimate}{Estimated probability, in percentages}
+#'   \item{starttime, endtime}{}
+#'   \item{RT}{}
+#' }
+#' @references
+#' \insertRef{zhu2020BayesianSamplerGeneric}{samplrData}
+"zhu2020e1"
+
+#' Data from Experiment 2 in \insertCite{zhu2020BayesianSamplerGeneric;textual}{samplrData}
+#'
+#' Participants made probability judgments of the format: “What is the probability that the weather is \[X\] on a random day in England?". Various weather events were used, and the queries included both marginal events, conditional events, conjunctions, and disjunctions. The total set of 20 unique queries formed a block within which the presentation order was randomized for each participant. The experiment consisted of three blocks, so that all participants responded to each unique query three times.
+#'
+#' This data are licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/), reproduced from materials in [OSF](https://osf.io/mgcxj/).
+#' \describe{
+#'   \item{ID}{}
+#'   \item{block}{3 blocks in total}
+#'   \item{trial}{Trial Number within a block}
+#'   \item{query, querydetail}{Verbal descriptions of the query}
+#'   \item{querytype}{Type of query: e.g. notBgA = p(¬B|A)}
+#'   \item{Estimate}{Estimated probability, in percentages}
+#'   \item{starttime, endtime}{}
+#'   \item{RT}{}
+#' }
+#' @references
+#' \insertRef{zhu2020BayesianSamplerGeneric}{samplrData}
+"zhu2020e2"
+
+
+
 #' Data from Experiment 3 in \insertCite{sundh2023UnifiedExplanationVariability;textual}{samplrData}
 #'
 #' Participants made probability judgments of the format: “What is the probability that the weather is \[X\] on a random day in England?". Various weather events were used, and the queries included both marginal events, conditional events, conjunctions, and disjunctions. The total set of 20 unique queries formed a block within which the presentation order was randomized for each participant. The experiment consisted of three blocks, so that all participants responded to each unique query three times.
